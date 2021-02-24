3c0e6c82dbe13e6a7160a9d69db72d17bb1f99e6
zac <doicirohangnan@gmail.com>
2021-02-24 17:29:43 -0500

    1 - Skeleton classes created

---
+++ src/model/Sorting.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Sorting {
+
+    ArrayList<ArrayList<Integer>> lineByLine = new ArrayList<>(); //[elements],[meanOfSorts]
+
+    public void readInput(String input) {
+
+    }
+
+    private void setLines(String[] splitInput) {
+
+    }
+
+    private void setMean() {
+
+    }
+
+    public ArrayList<ArrayList<Integer>> getLineByLine() {
+        return lineByLine;
+    }
+}
