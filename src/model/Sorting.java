2dcef3cfe7e0a2df27ca6241c1dec5cf41ada2f9
zac <doicirohangnan@gmail.com>
2021-02-24 18:39:27 -0500

    2 - Added 'data/kidslist.txt' - Loaded file successfully

--- src/model/Sorting.java
+++ src/model/Sorting.java
@@ -1,14 +1,15 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Sorting {
 
     ArrayList<ArrayList<Integer>> lineByLine = new ArrayList<>(); //[elements],[meanOfSorts]
-
-    public void readInput(String input) {
-
-    }
+    private int testCases;
 
     private void setLines(String[] splitInput) {
 
@@ -16,6 +17,18 @@ public class Sorting {
 
     private void setMean() {
 
+    }
+
+    public void load(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/data/" + fileName));
+        testCases = br.read();
+        String line = br.readLine();
+        while(line!=null){
+            String[] parts = line.split(" ");
+
+            line = br.readLine();
+        }
+        br.close();
     }
 
     public ArrayList<ArrayList<Integer>> getLineByLine() {
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,30 @@
 package ui;
 import model.*;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Sorting sorter = new Sorting();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("--------------------------Bubble Kids--------------------------");
+        System.out.println("$>: Specify input file (must be part of listed files below): ");
+        System.out.println("$>: Below are the available files located in \"/src/data\"");
+        listCommand();
+        System.out.println("$>: Please type the name of the file you want to chose.\n" +
+                " >: Incorrect file names may lead to unexpected errors: ");
+        sorter.load(in.readLine());
+        System.out.println("$>: Input is being read... ");
+
+    }
+
+    public static void listCommand() {
+        try {
+            new ProcessBuilder("powershell","/c","dir .\\src\\data\\").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 }
