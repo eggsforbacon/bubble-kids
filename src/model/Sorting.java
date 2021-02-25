856fe650973a20b4c493a7fa3601f735064b979f
zac <doicirohangnan@gmail.com>
2021-02-24 22:24:02 -0500

    5 - Decimal formatting added - Rounding is pending. Might change it, might not

--- src/model/Sorting.java
+++ src/model/Sorting.java
@@ -3,6 +3,7 @@ package model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Sorting {
@@ -51,14 +52,16 @@ public class Sorting {
 
     public String asString() {
         StringBuilder output = new StringBuilder();
+        DecimalFormat df = new DecimalFormat("#.##");
         for (ArrayList<Double> doubles: lineByLine) {
-            output.append(doubles.get(0));
+            output.append(df.format(doubles.get(0)));
             output.append("-");
-            for (double d: doubles) {
-                output.append(d).append(" ");
+            for (int i = 1, doublesSize = doubles.size(); i < doublesSize; i++) {
+                double d = doubles.get(i);
+                output.append(df.format(d)).append(" ");
             }
             output.deleteCharAt(output.length() - 1);
-            output.append("\n");
+            output.append("\n----------------------------------------------------------------------\n");
         }
         output.append("-------------------------------------------------------------").append("\n$>: Test cases: ").append(testCases);
         return output.toString();
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,29 +4,63 @@ import model.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Sorting sorter = new Sorting();
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner in = new Scanner(System.in);
+        clear();
         System.out.println("--------------------------Bubble Kids--------------------------");
         System.out.println("$>: Specify input file (must be part of listed files below): ");
         System.out.println("$>: Below are the available files located in \"/src/data\"");
         listCommand();
-        System.out.println("$>: Please type the name of the file you want to chose.\n" +
-                " >: Incorrect file names may lead to unexpected errors: ");
-        sorter.load(in.readLine());
-        System.out.println("$>: Input is being read... ");
+        System.out.println("$: Please type the name of the file you want to chose.\n" +
+                " > Incorrect file names may lead to unexpected errors: ");
+        sorter.load(br.readLine());
+        System.out.println("$: Input is being sorted... ");
         sorter.bubbleSort();
-        System.out.println("$>: Input sorted!");
-        System.out.println("$:> Printing output...");
+        System.out.println("$: Input sorted!");
+        wait(1000);
+        clear();
+        System.out.println("$: Printing output...");
+        wait(1000);
         System.out.println(sorter.asString());
-
+        System.out.println("$: Output printed!");
+        wait(1000);
+        System.out.print("$: Press any key to exit");
+        in.nextLine();
+        in.close();
+        clear();
+        System.out.println("$/: Terminated");
     }
 
     public static void listCommand() {
         try {
             new ProcessBuilder("powershell","/c","dir .\\src\\data\\").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void clear() {
+        try {
+            final String OS = System.getProperty("os.name");
+            if (OS.contains("Windows")) {
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            }
+            else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception e) {
+            System.out.println("Screen could not be cleared (" + e + ")\n");
+        }
+    }
+
+    public static void wait(int millis) {
+        try {
+            Thread.sleep(millis);
         } catch (Exception e) {
             e.getStackTrace();
         }
